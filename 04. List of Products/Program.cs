38f177758944df7a97aa5a56c44ba655451729a4
hasankr <hasankr@abv.bg>
2022-06-18 10:29:25 +0300

    added 4

---
+++ 04. List of Products/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._List_of_Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> product = new List<string>();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                product.Add(Console.ReadLine());
+            }
+            product.Sort();
+            int counter = 1;
+            foreach (var produc in product)
+            {
+                Console.WriteLine($"{counter}.{produc}");
+                counter++;
+            }
+        }
+    }
+}
